93d2e5fe05e72526f9e1c83fd3ca96b0a8d6e031
syq <shenyongqiang2008@126.com>
2018-12-03 17:08:30 +0800

    优化

---
+++ Gui/PlayerBaoJi/SSPlayerAmmoBaoJi.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SSPlayerAmmoBaoJi : MonoBehaviour
+{
+    /// <summary>
+    /// 暴击粒子对象.
+    /// </summary>
+    public ParticleSystem m_BaoJiParticle;
+
+    /// <summary>
+    /// 初始化.
+    /// </summary>
+    internal void Init(PlayerEnum indexPlayer)
+    {
+        if (m_BaoJiParticle != null)
+        {
+            int baoJiDengJi = 0;
+            if (XkGameCtrl.GetInstance().m_CaiPiaoHealthDt != null)
+            {
+                baoJiDengJi = XkGameCtrl.GetInstance().m_CaiPiaoHealthDt.GetPlayerBaoJiDengJi(indexPlayer);
+            }
+
+            if (baoJiDengJi <= 0)
+            {
+                m_BaoJiParticle.gameObject.SetActive(false);
+            }
+            else
+            {
+                SSDebug.Log("Init -> indexPlayer == " + indexPlayer + ", baoJiDengJi ============ " + baoJiDengJi);
+                Material mat = null;
+                if (XkGameCtrl.GetInstance().m_CaiPiaoHealthDt != null)
+                {
+                    mat = XkGameCtrl.GetInstance().m_CaiPiaoHealthDt.GetPlayerBaoJiMaterial(indexPlayer, baoJiDengJi);
+                }
+
+                if (mat == null)
+                {
+                    m_BaoJiParticle.gameObject.SetActive(false);
+                }
+                else
+                {
+                    m_BaoJiParticle.renderer.material = mat;
+                    m_BaoJiParticle.gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+}
