263fd08dbcae6f4562b259ca9d5bc3b967bcd065
syq <shenyongqiang2008@126.com>
2019-01-25 11:08:04 +0800

    添加注释

--- Server/WebSocket/SSBoxPostNet.cs
+++ Server/WebSocket/SSBoxPostNet.cs
@@ -1,6 +1,6 @@
 ﻿#define USE_TEST_HDD_SERVER //使用红点点测试服务器.
-//#define USE_OLD_GET_HDD_GAME_CONFIG
-//#define TEST_DAI_JIN_QUAN
+//#define USE_OLD_GET_HDD_GAME_CONFIG //使用旧版本后台数据管理接口.
+//#define TEST_DAI_JIN_QUAN //测试代金券.
 using Assets.XKGame.Script.Comm;
 using Assets.XKGame.Script.Server.WebSocket;
 using LitJson;
