a5d8efdc883c2d8cc73cec4e909b6196d64cb4fe
syq <shenyongqiang2008@126.com>
2019-02-02 16:33:51 +0800

    将JPBoss和战车Boss的删除触发器分开

--- Trigger/SSTriggerCamerBox.cs
+++ Trigger/SSTriggerCamerBox.cs
@@ -2,12 +2,51 @@
 
 public class SSTriggerCamerBox : MonoBehaviour
 {
+    public enum TriggerState
+    {
+        /// <summary>
+        /// JPBoss触发器.
+        /// </summary>
+        JPBoss = 0,
+        /// <summary>
+        /// 战车Boss触发器.
+        /// </summary>
+        ZhanCheBoss = 1,
+    }
+    public TriggerState m_TriggerState = TriggerState.JPBoss;
+
     void OnTriggerExit(Collider other)
     {
         XKNpcMoveCtrl npcMoveCom = other.gameObject.GetComponent<XKNpcMoveCtrl>();
         if (npcMoveCom != null && npcMoveCom.IsCaiPiaoZhanChe)
         {
             //Debug.Log("Unity: OnTriggerExit******************name === " + npcMoveCom.name);
+            bool isExit = false;
+            switch (m_TriggerState)
+            {
+                case TriggerState.JPBoss:
+                    {
+                        if (npcMoveCom.GetIsBossNpc() == true)
+                        {
+                            isExit = true;
+                        }
+                        break;
+                    }
+                case TriggerState.ZhanCheBoss:
+                    {
+                        if (npcMoveCom.GetIsBossNpc() == false)
+                        {
+                            isExit = true;
+                        }
+                        break;
+                    }
+            }
+
+            if (isExit == false)
+            {
+                return;
+            }
+
             if (npcMoveCom.GetIsBossNpc() == true)
             {
                 //Boss走出镜头范围.
@@ -39,7 +78,31 @@ public class SSTriggerCamerBox : MonoBehaviour
         {
             //Debug.Log("Unity: OnTriggerEnter******************name === " + npcMoveCom.name);
             //彩票战车和boss进入摄像机盒子.
-            npcMoveCom.SetIsEnterCameraBox();
+            bool isEnter = false;
+            switch (m_TriggerState)
+            {
+                case TriggerState.JPBoss:
+                    {
+                        if (npcMoveCom.GetIsBossNpc() == true)
+                        {
+                            isEnter = true;
+                        }
+                        break;
+                    }
+                case TriggerState.ZhanCheBoss:
+                    {
+                        if (npcMoveCom.GetIsBossNpc() == false)
+                        {
+                            isEnter = true;
+                        }
+                        break;
+                    }
+            }
+
+            if (isEnter == true)
+            {
+                npcMoveCom.SetIsEnterCameraBox();
+            }
         }
     }
 }
