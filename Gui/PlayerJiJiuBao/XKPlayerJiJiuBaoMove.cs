45c5f631782dad62bb0787c3c0756a9d44091bc5
syq <shenyongqiang2008@126.com>
2019-03-04 13:01:50 +0800

    修改加血补给ui提示逻辑

--- Gui/PlayerJiJiuBao/XKPlayerJiJiuBaoMove.cs
+++ Gui/PlayerJiJiuBao/XKPlayerJiJiuBaoMove.cs
@@ -17,25 +17,51 @@ public class XKPlayerJiJiuBaoMove : MonoBehaviour
 		twPos.to = startPos + new Vector3(0f, PiaoZiPY, 0f);
 		twPos.duration = PiaoZiTime;
 		twPos.PlayForward();
-		
-		TweenAlpha twAlp = gameObject.AddComponent<TweenAlpha>();
-		twAlp.from = 1f;
-		twAlp.to = 0f;
-		twAlp.duration = PiaoZiTime;
-		twAlp.PlayForward();
-		
-		EventDelegate.Add(twAlp.onFinished, delegate{
-			HiddenPlayerJiJiuBao();
-		});
-	}
-	
-	void HiddenPlayerJiJiuBao()
-	{
-        //TweenPosition twPos = gameObject.GetComponent<TweenPosition>();
-        //DestroyObject(twPos);
-        //TweenAlpha twAlp = gameObject.GetComponent<TweenAlpha>();
-        //DestroyObject(twAlp);
-        //gameObject.SetActive(false);
-        Destroy(gameObject);
-	}
+
+        EventDelegate.Add(twPos.onFinished, delegate {
+            StartCoroutine(StartPlayTweenAlpha());
+        });
+        m_TimeLast = Time.time;
+    }
+
+    /// <summary>
+    /// 开始播放淡化效果.
+    /// </summary>
+    System.Collections.IEnumerator StartPlayTweenAlpha()
+    {
+        yield return new WaitForSeconds(1f);
+        TweenAlpha twAlp = gameObject.AddComponent<TweenAlpha>();
+        twAlp.from = 1f;
+        twAlp.to = 0f;
+        twAlp.duration = 1f;
+        twAlp.PlayForward();
+    }
+        
+    void Update()
+    {
+        UpdateRemoveSelf();
+    }
+
+    bool IsRemoveSelf = false;
+    float m_TimeLast = 0f;
+    void UpdateRemoveSelf()
+    {
+        if (IsRemoveSelf == false)
+        {
+            if (Time.time - m_TimeLast >= 2f + PiaoZiTime)
+            {
+                m_TimeLast = Time.time;
+                RemoveSelf();
+            }
+        }
+    }
+
+    internal void RemoveSelf()
+    {
+        if (IsRemoveSelf == false)
+        {
+            IsRemoveSelf = true;
+            Destroy(gameObject);
+        }
+    }
 }
