5edb44bd52ac0c02fe208383fbc165c07038efe5
syq <shenyongqiang2008@126.com>
2019-03-19 15:53:16 +0800

    修改版本号为20190319.01

--- SetPanel/XKGameVersionCtrl.cs
+++ SetPanel/XKGameVersionCtrl.cs
@@ -3,7 +3,7 @@
 public class XKGameVersionCtrl : MonoBehaviour
 {
     UILabel VersionLB;
-    static string _GameVersion = "Version: 20190318.01";
+    static string _GameVersion = "Version: 20190319.01";
     public static string GameVersion
     {
         get
